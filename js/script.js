5d3f1781b3833db5150dff5f424d0fd5753ba0cf
Cesar Diaz <cesararielignaciodiaz@gmail.com>
2023-03-01 17:44:49 -0300

    Se agrega el Proyecto

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// menu emergente
+var menu_visible = false
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+//barra de porcentaje
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
